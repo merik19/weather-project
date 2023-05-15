852f31a995575971a9489d53d7acec0179d67317
merik19 <merik19@yahoo.com>
2023-05-15 22:49:06 +0300

    Changed JS weather elements
    temperature, wind, humidity

--- index.js
+++ index.js
@@ -1,71 +1,76 @@
-function formatDate(date) {
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  let dayIndex = date.getDay();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[dayIndex];
+//function formatDate(date) {
+//  let hours = date.getHours();
+//  if (hours < 10) {
+//    hours = `0${hours}`;
+//  }
+//  let minutes = date.getMinutes();
+//  if (minutes < 10) {
+//    minutes = `0${minutes}`;
+//  }
+//  let dayIndex = date.getDay();
+//  let days = [
+//    "Sunday",
+//    "Monday",
+//    "Tuesday",
+//    "Wednesday",
+//    "Thursday",
+//   "Friday",
+//    "Saturday",
+//  ];
+//  let day = days[dayIndex];
 
-  return `${day} ${hours}:${minutes}`;
-}
+//  return `${day} ${hours}:${minutes}`;
+//}
 
 function displayWeatherCondition(response) {
   console.log(response.data);
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
+
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
-function searchCity(city) {
-  let apiKey = "e2ca49f28ba267a0a3202ae6eebab6dd";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeatherCondition);
-}
+let apiKey = "e2ca49f28ba267a0a3202ae6eebab6dd";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayWeatherCondition);
 
-function handleSubmit(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-input").value;
-  searchCity(city);
-}
+//function searchCity(city) {
+//  let apiKey = "e2ca49f28ba267a0a3202ae6eebab6dd";
+//  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+//  axios.get(apiUrl).then(displayWeatherCondition);
+//}
 
-function searchLocation(position) {
-  let apiKey = "e2ca49f28ba267a0a3202ae6eebab6dd";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeatherCondition);
-}
+//function handleSubmit(event) {
+//  event.preventDefault();
+//  let city = document.querySelector("#city-input").value;
+//  searchCity(city);
+//}
 
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
+//function searchLocation(position) {
+//  let apiKey = "e2ca49f28ba267a0a3202ae6eebab6dd";
+//  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+//  axios.get(apiUrl).then(displayWeatherCondition);
+//}
 
-let dateElement = document.querySelector("#date");
-let currentTime = new Date();
-let searchForm = document.querySelector("#search-form");
+//function getCurrentLocation(event) {
+//  event.preventDefault();
+//  navigator.geolocation.getCurrentPosition(searchLocation);
+//}
 
-searchForm.addEventListener("submit", handleSubmit);
+//let dateElement = document.querySelector("#date");
+//let currentTime = new Date();
+//let searchForm = document.querySelector("#search-form");
 
-dateElement.innerHTML = formatDate(currentTime);
+//searchForm.addEventListener("submit", handleSubmit);
 
-let currentLocationButton = document.querySelector("#current-location-button");
-currentLocationButton.addEventListener("click", getCurrentLocation);
+//dateElement.innerHTML = formatDate(currentTime);
 
-searchCity("Kyiv");
+//let currentLocationButton = document.querySelector("#current-location-button");
+//currentLocationButton.addEventListener("click", getCurrentLocation);
+
+//searchCity("Kyiv");
