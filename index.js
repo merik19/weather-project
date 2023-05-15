66062111fe1070f0239b57a1212f0e5312c53d07
merik19 <merik19@yahoo.com>
2023-05-15 23:43:47 +0300

    Changes icon

--- index.js
+++ index.js
@@ -25,25 +25,31 @@ function formatDate(date) {
 function displayWeatherCondition(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let currentTime = new Date();
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(currentTime);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "e2ca49f28ba267a0a3202ae6eebab6dd";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayWeatherCondition);
 
-
-
 //let searchForm = document.querySelector("#search-form");
-
 
 //function searchCity(city) {
 //  let apiKey = "e2ca49f28ba267a0a3202ae6eebab6dd";
@@ -68,11 +74,7 @@ axios.get(apiUrl).then(displayWeatherCondition);
 //  navigator.geolocation.getCurrentPosition(searchLocation);
 //}
 
-
-
 //searchForm.addEventListener("submit", handleSubmit);
-
-
 
 //let currentLocationButton = document.querySelector("#current-location-button");
 //currentLocationButton.addEventListener("click", getCurrentLocation);
