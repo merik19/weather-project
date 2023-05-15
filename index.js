b0329175f207313cd52e70ed02f2718ca6b5fa23
merik19 <merik19@yahoo.com>
2023-05-15 23:18:56 +0300

    JS moved positions

--- index.js
+++ index.js
@@ -23,26 +23,27 @@ function formatDate(date) {
 }
 
 function displayWeatherCondition(response) {
-  console.log(response.data);
-
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let currentTime = new Date();
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(currentTime);
 }
 
 let apiKey = "e2ca49f28ba267a0a3202ae6eebab6dd";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayWeatherCondition);
 
-let dateElement = document.querySelector("#date");
-let currentTime = new Date();
-let searchForm = document.querySelector("#search-form");
-dateElement.innerHTML = formatDate(currentTime);
+
+
+//let searchForm = document.querySelector("#search-form");
+
 
 //function searchCity(city) {
 //  let apiKey = "e2ca49f28ba267a0a3202ae6eebab6dd";
