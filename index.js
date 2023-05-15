9577a9fcc018c0ebb9600e924aa264e8040089b2
merik19 <merik19@yahoo.com>
2023-05-15 23:15:14 +0300

    Updated time

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-//function formatDate(date) {
-//  let hours = date.getHours();
-//  if (hours < 10) {
-//    hours = `0${hours}`;
-//  }
-//  let minutes = date.getMinutes();
-//  if (minutes < 10) {
-//    minutes = `0${minutes}`;
-//  }
-//  let dayIndex = date.getDay();
-//  let days = [
-//    "Sunday",
-//    "Monday",
-//    "Tuesday",
-//    "Wednesday",
-//    "Thursday",
-//   "Friday",
-//    "Saturday",
-//  ];
-//  let day = days[dayIndex];
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
 
-//  return `${day} ${hours}:${minutes}`;
-//}
+  return `${day} ${hours}:${minutes}`;
+}
 
 function displayWeatherCondition(response) {
   console.log(response.data);
@@ -38,6 +38,11 @@ function displayWeatherCondition(response) {
 let apiKey = "e2ca49f28ba267a0a3202ae6eebab6dd";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayWeatherCondition);
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+let searchForm = document.querySelector("#search-form");
+dateElement.innerHTML = formatDate(currentTime);
 
 //function searchCity(city) {
 //  let apiKey = "e2ca49f28ba267a0a3202ae6eebab6dd";
@@ -62,13 +67,11 @@ axios.get(apiUrl).then(displayWeatherCondition);
 //  navigator.geolocation.getCurrentPosition(searchLocation);
 //}
 
-//let dateElement = document.querySelector("#date");
-//let currentTime = new Date();
-//let searchForm = document.querySelector("#search-form");
+
 
 //searchForm.addEventListener("submit", handleSubmit);
 
-//dateElement.innerHTML = formatDate(currentTime);
+
 
 //let currentLocationButton = document.querySelector("#current-location-button");
 //currentLocationButton.addEventListener("click", getCurrentLocation);
