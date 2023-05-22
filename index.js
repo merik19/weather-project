874f2d046f8b6f682be3c2113e142b5d38f88406
merik19 <merik19@yahoo.com>
2023-05-22 17:32:03 +0300

    Add API

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecastBlock() {
+function displayForecastBlock(response) {
+  console.log(response.data);
   let forecastBlockElement = document.querySelector("#forecast-block");
 
   let forecastHTML = `<div class="row">`;
@@ -50,6 +51,13 @@ function displayForecastBlock() {
   forecastBlockElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "1dbf926d3b4417bf379db7043bec1047";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecastBlock);
+}
+
 function displayWeatherCondition(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -73,6 +81,8 @@ function displayWeatherCondition(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -126,4 +136,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Madrid");
-displayForecastBlock();
